944fcccfeb785bec8072288b126c90fbe902d4a6
YTkauagamer <87506737+YTkauagamer@users.noreply.github.com>
2026-02-08 00:50:22 -0300

    Update commands.js

--- commands.js
+++ commands.js
@@ -1,5 +1,6 @@
 const commands = {
   "!dog": "https://media.giphy.com/media/3oriO0OEd9QIDdllqo/giphy.gif",
   "!cat": "https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif",
-  "!gg": "https://i.imgur.com/9RM84YB.png"
+  "!gg": "https://i.imgur.com/9RM84YB.png",
+  "!love": "https://media.giphy.com/media/l4FGuhL4U2WyjdkaY/giphy.gif"
 };
